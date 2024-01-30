78009f73c8642dff0a196ff340bfdc1b0eda75f3
SilvioSFS <158211885+SilvioSFS@users.noreply.github.com>
2024-01-30 20:54:40 -0300

    Atividade 1.java

--- Atividade 1.java
+++ Atividade 1.java
@@ -1,42 +1,47 @@
-    class carro{
-        private String Marca;
-        private String Modelo;
-        private int ano;
-        private char cor;
-        private float velatual;
-    }
-    
-    Carro(String Marca, String Modelo, int ano, char cor, float velatual) {
+class Carro {
+    private String Marca;
+    private String Modelo;
+    private int ano;
+    private String cor;
+    private float velatual;
+
+    public Carro(String Marca, String Modelo, int ano, String cor, float velatual) {
         this.Marca = Marca;
         this.Modelo = Modelo;
         this.ano = ano;
-        this. cor = cor;
+        this.cor = cor;
         this.velatual = velatual;
     }
+
     public String getMarca() {
         return Marca;
     }
+
     public String getModelo() {
         return Modelo;
     }
+
     public int getano() {
         return ano;
     }
-    public char getcor() {
+
+    public String getcor() {
         return cor;
     }
-    public float getvelatul() {
+
+    public float getvelatual() {
         return velatual;
     }
-    public class Main {
-        public static void main(String[] args) {
-            Carro carro1 = new Carro("Chevrolet", "Impala", 1967, preto, 180);
-            
-            System.out.println("Marca: " + carro1.getMarca()); 
-            System.out.println("Modelo: " + carro1.getModelo()); 
-            System.out.println("Ano: " + carro1.getano()); 
-            System.out.println("Cor: " + carro1.getcor()); 
-            System.out.println("Velocidade Atual: " + carro1.velatual()); 
-        }
-    }
+}
 
+public class Main {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro("Chevrolet", "Impala", 1967, "Preto", 180);
+
+        System.out.printf("Marca: %s\n", carro1.getMarca());
+        System.out.printf("Modelo: %s\n", carro1.getModelo());
+        System.out.printf("Ano: %d\n", carro1.getano());
+        System.out.printf("Cor: %s\n", carro1.getcor());
+        System.out.printf("Velocidade Atual: %.1f\n", carro1.getvelatual());
+    }
+}
